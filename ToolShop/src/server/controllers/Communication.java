933d01984e2cefb7de0a52f1988e09301833aa8d
Jofred Cayabyab <jjay.cayabyab@gmail.com>
2019-04-01 14:50:42 -0600

    add server handling for client requests

--- ToolShop/src/server/controllers/Communication.java
+++ ToolShop/src/server/controllers/Communication.java
@@ -1,12 +1,15 @@
 package server.controllers;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import utils.DataCodes;
+import server.models.*;
 
 /**
  * Allows for communication between the server and the client as well as create
@@ -43,6 +46,8 @@ public class Communication implements DataCodes {
    */
   ObjectOutputStream socketOut;
 
+  ToolShop theShop;
+
   /**
    * constructs the server and initializes the client and server connection
    * 
@@ -54,7 +59,7 @@ public class Communication implements DataCodes {
       System.out.println("Server is now running.");
       aSocket = serverSocket.accept();
       System.out.println("A client has connected.");
-      socketOut = new ObjectOutputStream(aSocket.getOutputStream()); 
+      socketOut = new ObjectOutputStream(aSocket.getOutputStream());
       socketIn = new ObjectInputStream(aSocket.getInputStream());
     } catch (IOException e) {
       // TODO: Create formal error handling
@@ -74,35 +79,76 @@ public class Communication implements DataCodes {
         case SEND_USERDATA: {
           System.out.println("Received " + SEND_USERDATA);
           // TODO: Change this test input
-          System.out.println((String) socketIn.readObject());
+          UserInformation user = (UserInformation) socketIn.readObject();
           break;
         }
         case GET_TOOLS: {
           System.out.println("Received " + GET_TOOLS);
+          ArrayList<Item> toolList = theShop.getItems().getList();
+          socketOut.writeObject(toolList);
           break;
         }
         case SEARCH_TOOL_NAME: {
           System.out.println("Received " + SEARCH_TOOL_NAME);
+          String name = (String) socketIn.readObject();
+          Item item = searchToolName(name);
+          if (item == null) {
+            socketOut.writeObject(SEND_ERROR);
+          } else {
+            socketOut.writeObject(item);
+          }
           break;
         }
         case SEARCH_TOOL_ID: {
           System.out.println("Received " + SEARCH_TOOL_ID);
+          try {
+            int id = Integer.parseInt((String) socketIn.readObject());
+            Item item = searchToolId(id);
+            if (item == null) {
+              socketOut.writeObject(SEND_ERROR);
+            } else {
+              socketOut.writeObject(item);
+            }
+          } catch (NumberFormatException e) {
+            socketOut.writeObject(SEND_ERROR);
+          }
           break;
         }
         case CREATE_ITEM: {
           System.out.println("Received " + CREATE_ITEM);
+          String description = (String) socketIn.readObject();
+          int quantity = Integer.parseInt((String) socketIn.readObject());
+          double price = Double.parseDouble((String) socketIn.readObject());
+          int supplierId = Integer.parseInt((String) socketIn.readObject());
+          Item newItem = addNewTool(description, quantity, price, supplierId);
+          if (newItem == null) {
+            socketOut.writeObject(SEND_ERROR);
+          } else {
+            socketOut.writeObject(newItem);
+          }
           break;
         }
         case DELETE_ITEM: {
           System.out.println("Received " + DELETE_ITEM);
+          Item itemToDelete = (Item) socketIn.readObject();
+          deleteTool(itemToDelete);
+          socketOut.writeObject(SEND_SUCCESS);
           break;
         }
         case ORDER_ITEM: {
           System.out.println("Received " + ORDER_ITEM);
+          ArrayList<Item> toolList = theShop.getItems().getList();
+          socketOut.writeObject(toolList);
+          checkItemQuantity();
+
           break;
         }
         case DECREASE_ITEM: {
           System.out.println("Received " + DECREASE_ITEM);
+          Item itemToDecrease = (Item) socketIn.readObject();
+          int count = Integer.parseInt((String) socketIn.readObject());
+          decreaseItemQuantity(itemToDecrease, count);
+          socketOut.writeObject(SEND_SUCCESS);
           break;
         }
         case QUIT: {
@@ -122,6 +168,80 @@ public class Communication implements DataCodes {
         e.printStackTrace();
       }
     }
+  }
+
+  /**
+   * Lists all of the tools held within the shop.
+   */
+  private void listTools() {
+    System.out.println(theShop.getItems());
+  }
+
+  /**
+   * Asks the user for a name of an object, then prints the object information to
+   * the console, then returns the object.
+   * 
+   * @return The Item if it exists, otherwise returns null
+   * @throws IOException
+   */
+  private Item searchToolName(String name) {
+    Item item = theShop.getItems().getItemByName(name);
+    return item;
+  }
+
+  /**
+   * Asks the user for an ID of an object, the prints the object information to
+   * the console, then returns the object
+   * 
+   * @return The Item if it exists, otherwise returns null
+   * @throws IOException
+   */
+  private Item searchToolId(int id) {
+    Item item = theShop.getItems().getItemById(id);
+
+    return item;
+  }
+
+  /**
+   * Checks all of the item stock in the shop then automatically generates an
+   * order if items need to be ordered.
+   * 
+   * @throws FileNotFoundException
+   */
+  private void checkItemQuantity() throws FileNotFoundException {
+    System.out.println("Inventory being checked...");
+    if (theShop.checkStock()) {
+      System.out.println("An order has been generated. Please check the orders.txt log for the new order.");
+    } else {
+      System.out.println("All items have enough stock.");
+    }
+  }
+
+  /**
+   * Asks the user for an item to decrease the quantity.
+   * 
+   * @throws IOException
+   */
+  private void decreaseItemQuantity(Item toDecrease, int count) throws IOException {
+    theShop.buy(toDecrease, count);
+  }
+
+  /**
+   * Asks the user for information about a new tool, then generates a new tool in
+   * the ItemList.
+   */
+  public Item addNewTool(String description, int quantity, double price, int supplierId) {
+      Item newItem = theShop.addNewItem(description, quantity, price, supplierId);
+      return newItem;
+  }
+
+  /**
+   * Asks the user for a tool, then deletes the tool from the shop inventory.
+   * 
+   * @throws IOException
+   */
+  public void deleteTool(Item toDelete) throws IOException {
+    theShop.getItems().deleteItem(toDelete);
   }
 
   public static void main(String[] args) {
