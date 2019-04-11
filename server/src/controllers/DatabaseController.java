4d09fd4cb43db1994a350221e2031db12b0cba2a
Tyler Lam <tylerl264@gmail.com>
2019-04-10 23:28:09 -0600

    amazing

--- server/src/controllers/DatabaseController.java
+++ server/src/controllers/DatabaseController.java
@@ -1,66 +1,75 @@
 package controllers;
+import com.mysql.jdbc.Driver;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.sql.*;
+import java.util.ArrayList;
+import models.*;
+
 import utils.*;
 
-public class DatabaseController {
+public class DatabaseController implements DBCredentials{
 	private Connection connection;
-	private Statement stmt;
-	private ResultSet rs;
-	private String ADD_ITEM = "INSERT INTO item (id, description, quantity, price, supplierID) values(?,?,?,?,?)";
-	private String DELETE_ITEM = "DELETE FROM item WHERE id=?";
-	private String GET_ITEM_BY_DESCRIPTION = "SELECT * FROM item WHERE description=?";
-	private String GET_ITEM_BY_ID = "SELECT * FROM item WHERE id=?";
-	private String BUY_ITEM = "UPDATE item SET quantity=? WHERE id=?";
-	private String CHECK_USER = "SELECT * FROM logins WHERE username=? and password=?";
-	private String ADD_USER = "INSERT INTO logins (username, password) values(?,?)";
 
 	public DatabaseController() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/toolshop?user=root", "root", "root");
-			stmt = connection.createStatement();
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/toolshop", "root", "root");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	public void close() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public void addUser(UserInformation user) {
 		try {
 			PreparedStatement newUser = connection.prepareStatement(ADD_USER);
 			newUser.setString(1, user.getId());
 			newUser.setString(2, user.getPassword());
 			newUser.execute();
-		} catch(SQLException e) {
+			newUser.close();
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public boolean checkUser(UserInformation user) {
 		try {
 			PreparedStatement findUser = connection.prepareStatement(CHECK_USER);
 			findUser.setString(1, user.getId());
 			findUser.setString(2, user.getPassword());
 			ResultSet rs = findUser.executeQuery();
-			if(rs.next()) {
+			if (rs.next()) {
 				return true;
 			}
-		} catch(SQLException e) {
+			findUser.close();
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return false;
 	}
-	
+
 	public int getIdByDescription(String description) {
 		try {
 			PreparedStatement searchByDescriptionStatement = connection.prepareStatement(GET_ITEM_BY_DESCRIPTION);
 			searchByDescriptionStatement.setString(1, description);
 			ResultSet rs = searchByDescriptionStatement.executeQuery();
-			if(rs.next()) {
+			if (rs.next()) {
 				return rs.getInt(1);
 			}
-		} catch(SQLException e) {
+			searchByDescriptionStatement.close();
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return -1;
@@ -76,6 +85,7 @@ public class DatabaseController {
 			addItemStatement.setInt(5, item.getSupplier().getId());
 
 			addItemStatement.execute();
+			addItemStatement.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -86,47 +96,51 @@ public class DatabaseController {
 		try {
 			PreparedStatement deleteStatement = connection.prepareStatement(query);
 			deleteStatement.execute();
+			deleteStatement.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void deleteItem(Item item) {
 		try {
-		PreparedStatement deleteItemStatement = connection.prepareStatement(DELETE_ITEM);
-		deleteItemStatement.setInt(1, item.getId());
-		deleteItemStatement.execute();
-		} catch(SQLException e) {
+			PreparedStatement deleteItemStatement = connection.prepareStatement(DELETE_ITEM);
+			deleteItemStatement.setInt(1, item.getId());
+			deleteItemStatement.execute();
+			deleteItemStatement.close();
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public int getItemById(int id) {
 		try {
 			PreparedStatement searchByIdStatement = connection.prepareStatement(GET_ITEM_BY_ID);
 			searchByIdStatement.setInt(1, id);
 			ResultSet rs = searchByIdStatement.executeQuery();
-			if(rs.next()) {
+			if (rs.next()) {
 				return rs.getInt(1);
 			}
-		} catch(SQLException e) {
+			searchByIdStatement.close();
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return -1;
 	}
-	
+
 	public void buyItem(Item itemToDecrease, int count) {
-		if(itemToDecrease.getQuantity() < count) {
+		if (itemToDecrease.getQuantity() < count) {
 			deleteItem(itemToDecrease);
 			return;
 		}
 		try {
 			PreparedStatement buy = connection.prepareStatement(BUY_ITEM);
-			int finalQuantity = itemToDecrease.getQuantity() -count;
-			buy.setInt(1,finalQuantity);
-			buy.setInt(2,itemToDecrease.getId());
+			int finalQuantity = itemToDecrease.getQuantity() - count;
+			buy.setInt(1, finalQuantity);
+			buy.setInt(2, itemToDecrease.getId());
 			buy.execute();
-		} catch(SQLException e) {
+			buy.close();
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
---
+++ server/src/utils/DBCredentials.java
@@ -0,0 +1,40 @@
+package utils;
+
+
+public interface DBCredentials {
+	/**
+	 * allows user to add item to the SQL database
+	 */
+	public static final String ADD_ITEM = "INSERT INTO item (id, description, quantity, price, supplierID) values(?,?,?,?,?)";
+	
+	/**
+	 * deletes a item from the SQL database
+	 */
+	public static final String DELETE_ITEM = "DELETE FROM item WHERE id=?";
+	
+	/**
+	 * searches for an item from the SQL database through description
+	 */
+	public static final String GET_ITEM_BY_DESCRIPTION = "SELECT * FROM item WHERE description=?";
+	
+	/**
+	 * searches for a item from the SQL database through ID
+	 */
+	public static final String GET_ITEM_BY_ID = "SELECT * FROM item WHERE id=?";
+	
+	/**
+	 * user can purchase an item
+	 */
+	public static final String BUY_ITEM = "UPDATE item SET quantity=? WHERE id=?";
+	
+	/**
+	 * checks the user information to see if its in the database
+	 */
+	public static final String CHECK_USER = "SELECT * FROM logins WHERE username=? and password=?";
+	
+	/**
+	 * adds a user into the pre-existing database
+	 */
+	public static final String ADD_USER = "INSERT INTO logins (username, password) values(?,?)";
+}
+
